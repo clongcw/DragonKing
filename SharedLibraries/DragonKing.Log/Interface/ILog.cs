a4ef4c4d6d51a735d45cb887df40e1aa80865148
c longc <chucl@innowavedx.com>
2023-07-11 11:43:55 +0800

    修复日志库无法上传的问题

---
+++ SharedLibraries/DragonKing.Log/Interface/ILog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonKing.Log.Interface
+{
+    public interface ILog
+    {
+        void Debug(string message);
+
+        void Debug(string messageTemplate, params object[] args);
+
+        void Information(string message);
+
+        void Information(string messageTemplate, params object[] args);
+
+        void Warning(string message);
+
+        void Warning(string messageTemplate, params object[] args);
+
+        void Error(Exception exception, string messageTemplate);
+
+        void Error(Exception exception, string messageTemplate, params object[] args);
+    }
+}
